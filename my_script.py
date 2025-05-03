c3ff9047089eea3a549a15245118f1cb9446c91d
Uriah Erter <uriah.erter@outlook.com>
2025-05-03 09:57:22 -0500

    Commented out the previous days exercises and updated the script name.

---
+++ my_script.py
@@ -0,0 +1,22 @@
+# ** day_1 script_1 **
+# print('Hello, world!')
+
+# # ** day_2 Exercise 1: Calculation **
+# milk = 3.19
+# bread = 2.58
+# eggs = 4.53
+
+# total = milk + bread + eggs
+# print(f'Your total is: ${total:.2f}')
+
+# # ** day_2 Exercise 2: String Manipulation **
+# store_name = input('What is your favorite grocery store? ')
+# print(f'Welcome to {store_name}!')
+
+# # ** day_2 Exercise 3: Common Errors
+# milk = 3
+# bread = 2.50
+# total = milk + bread
+# formatted_total = f'{total:.2f}'
+# print("The total cost is: $" + str(formatted_total))
+
